2e41c40973994da9d2fec09efa0fcb06d76319fd
Estevao Hilst <xtevan@gmail.com>
2016-11-29 17:24:46 -0200

    Added function

--- chart.py
+++ chart.py
@@ -4,13 +4,23 @@ import matplotlib.mlab as mlab
 import math
 from scipy.stats import uniform
 
+def arran(x, y):
+    for i in x:
+        print(i)
+        a = i * y
+        print(" " + a)
+        i = a
+    return x
+
 mu = 2
 variance = 1
 sigma = math.sqrt(variance)
 x = np.linspace(-3, 6, 100)
+y = mlab.normpdf(x,mu,sigma)
 plt.plot(x,mlab.normpdf(x,mu,sigma))
+plt.plot(x,arran(y,0.4),'r-')
 
 xn = np.linspace(uniform.ppf(0.01), uniform.ppf(0.99),100)
-plt.plot(xn,uniform.pdf(xn), 'r-', lw=5, alpha=0.6, label = 'uniform pdf')
+plt.plot(xn,uniform.pdf(xn), 'r-', lw=2, alpha=0.6, label = 'uniform pdf')
 
 plt.show()
